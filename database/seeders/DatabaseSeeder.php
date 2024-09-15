fd030867fc739ee6e5c4ac0259c0199d2ee39493
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-15 14:57:42 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-       EmployeesTableSeeder::run();
-       UsersTableSeeder::run();
-       EmployeeSeeder::run();
+       new EmployeesTableSeeder()::run();
+       new UsersTableSeeder()::run();
+       new EmployeeSeeder()::run();
     }
 }
