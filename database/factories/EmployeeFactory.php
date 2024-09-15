ba73e10cb8c1e53d83d4d78c955a520b9378cab5
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-15 15:10:19 +0300

    Update EmployeeFactory.php

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -54,9 +54,9 @@ class EmployeeFactory extends Factory
             'emergency_contact' => $this->faker->phoneNumber,
             'cv' => null,
             'image' => null,
-            'position_id' => function () {
+            'position_id' =>1/* function () {
                 return \App\Models\Position::inRandomOrder()->first()->id;
-            },
+            }*/,
             'training' => $this->faker->boolean,
             'schedule_id' => null,
         ];
