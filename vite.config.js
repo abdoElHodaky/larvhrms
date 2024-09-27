c15a03f572d816963cc70124f79a3b635032ca72
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-27 16:11:14 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,67 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
     plugins: [
+        VitePWA({
+           registerType:"auto",
+            workbox: {
+	globDirectory: 'public/',
+	globPatterns: [
+		'**/*.{css,png,jpg,ico,html,js,txt}'
+	],
+	navigateFallback:"/offline.html",
+	runtimeCaching: [{
+    urlPattern: ({request, url}) =>url.includes("cdn")==true,
+    handler: 'NetworkFirst',
+    options: {
+      cacheName: 'cdns',
+      expiration: {
+        maxEntries: 20,
+	maxAgeSeconds: 2 * 24 * 60 * 60,
+      },
+      cacheableResponse:{
+	statuses: [0, 200]
+      }
+    },
+  },{
+    urlPattern: ({request, url}) => request.method=="POST",
+    handler:"NetworkOnly",
+    method:"POST",
+    options:{
+      cacheName:"apCachePost",
+      cacheableResponse:{
+	statuses: [0, 200]
+      },
+      backgroundSync:{
+       name:"Apisync",
+       options:{
+    	maxRetentionTime:24*60*2
+       }
+      }
+    }
+  },{
+    urlPattern: ({request, url}) =>url.includes("fonts")==true,
+    handler: 'NetworkFirst',
+    options: {
+      cacheName: 'cdns',
+      expiration: {
+        maxEntries: 20,
+	maxAgeSeconds: 2 * 24 * 60 * 60,
+      },
+      cacheableResponse:{
+	statuses: [0, 200]
+      }
+    }
+  }],
+   swDest: 'public/sw.js'
+}}),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
