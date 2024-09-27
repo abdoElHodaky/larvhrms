ab78572b2cffe0fa1f607de6c7f7b1df7e8e6508
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-27 17:29:20 +0300

    Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="theme-color" content="#ef34ad">
     <title>{{ $title ?? 'HRMS' }}</title>
+    <link rel="manifest" href="{{ asset('/manifest.json') }}" >
     <link rel="preload" href="{{ asset('css/bootstrap.css') }}" as="style" onload="this.rel='stylesheet'">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}" media="print" onload="this.media='all'">
     <noscript><link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}"></noscript>
