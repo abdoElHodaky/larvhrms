111a1826b1040b1bb75a21a986cca0458c04aa87
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-10-01 17:32:58 +0300

    Update fly.sh

--- fly.sh
+++ fly.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 
-echo 'export FLYCTL_INSTALL=/home/abdo_arh36/.fly' >> /home/abdo_arh36/.bashrc;
-echo 'export PATH=$FLYCTL_INSTALL/bin:$PATH' >> /home/abdo_arh36/.bashrc;
+#echo 'export FLYCTL_INSTALL=/home/abdo_arh36/.fly' >> /home/abdo_arh36/.bashrc;
+#echo 'export PATH=$FLYCTL_INSTALL/bin:$PATH' >> /home/abdo_arh36/.bashrc;
+
+echo 'export COMPOSEGEN_INSTALL=/home/abdo_arh36/compose-generator' >> /home/abdo_arh36/.bashrc;
+echo 'export PATH=$COMPOSEGEN_INSTALL/bin:$PATH' >> /home/abdo_arh36/.bashrc;
